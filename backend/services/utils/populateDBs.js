b5b2fdda6a40545b1e640b11dcef577b4ab50f7a
nlemberg <naama.lemberg@gmail.com>
2022-05-31 16:19:41 +0300

    fixed typo in prod server url in populateDBs

--- backend/services/utils/populateDBs.js
+++ backend/services/utils/populateDBs.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const serverURL =
   process.env.NODE_ENV === "production"
-    ? "https://smc-services-dev.herokuapp.com/"
+    ? "https://smc-services-dev.herokuapp.com"
     : "http://localhost:8000";
 
 // const membersURL = "https://jsonplaceholder.typicode.com/users";
