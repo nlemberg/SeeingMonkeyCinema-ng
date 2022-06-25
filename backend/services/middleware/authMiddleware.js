6aeb61368542e5c550fcd5bf37a278119f661041
nlemberg <naama.lemberg@gmail.com>
2022-06-25 22:08:34 +0300

    set up authentication for employees

---
+++ backend/services/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const Employee = require("../models/employeeModel");
+
+const auth = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // get token from header (format: Bearer <token>)
+      token = req.headers.authorization.split(" ")[1];
+
+      // verify token
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET || require("../config/keys").JWT_SECRET
+      );
+
+      // get employee from token
+      req.employee = await Employee.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Unauthorized, no token");
+  }
+});
+
+module.exports = { auth };
